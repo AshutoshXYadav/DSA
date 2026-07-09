c6e1d58bc554d161a1bfaf964ff78309258678a1
Ashutosh Yadav <ashutoshyadav9872@gmail.com>
2026-07-09 11:28:28 +0530

    Time: 89 ms (34.59%), Space: 148.5 MB (92.71%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,28 @@
+#include <queue>
+
+class MedianFinder {
+private:
+    std::priority_queue<int> small; 
+    std::priority_queue<int, std::vector<int>, std::greater<int>> large; 
+
+public:
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        small.push(num);
+        large.push(small.top());
+        small.pop();
+        
+        if (small.size() < large.size()) {
+            small.push(large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (small.size() > large.size()) {
+            return small.top();
+        }
+        return (small.top() + large.top()) / 2.0;
+    }
+};
